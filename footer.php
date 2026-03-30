08d9733ae54fd1d1aef37c7bdf1b68c13d8dcf31
agarchali <agarchal@student.fitchburgstate.edu>
2026-03-30 10:10:20 -0400

    Update footer.php
    trello 3 spring 1. 1.2

--- footer.php
+++ footer.php
@@ -1,7 +1,53 @@
-    </div>
-    <footer class="w3-container w3-teal w3-center w3-margin-top">
-        <p>Copyright © 2026 RacingWiki</p>
-        <a href="#">Contact Us</a> | <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+<div style="
+    width: 100vw;
+    margin-left: calc(50% - 50vw);
+    background: #009688;
+    color: white;
+    margin-top: 20px;
+">
+    <footer style="text-align: center; padding: 16px 20px; margin: 0;">
+        <p style="margin: 0;">Copyright © 2026 RacingWiki</p>
     </footer>
-</body>
-</html>
+
+    <div style="
+        display: flex;
+        justify-content: space-between;
+        gap: 30px;
+        padding: 20px;
+        flex-wrap: wrap;
+        box-sizing: border-box;
+    ">
+        <div style="flex: 1; min-width: 200px; text-align: left;">
+            <p class="bottom-underline" style="margin: 0;">Contact us</p>
+            <div style="margin-top: 20px;">Phone: +1234567890</div>
+            <div style="margin-top: 20px;">Email: example@example.com</div>
+        </div>
+
+        <div style="flex: 1; min-width: 200px; text-align: left;">
+            <p class="bottom-underline" style="margin: 0;">Social media</p>
+            <div style="margin-top: 15px;">
+                <a href="https://www.youtube.com/" class="w3-button">Youtube</a>
+            </div>
+            <div>
+                <a href="https://www.instagram.com/" class="w3-button">Instagram</a>
+            </div>
+        </div>
+
+        <div style="flex: 1; min-width: 200px; text-align: left;">
+            <p class="bottom-underline" style="margin: 0;">About Us</p>
+            <div style="margin-top: 15px;">
+                <a href="about.php" class="w3-button">Project</a>
+            </div>
+        </div>
+
+        <div style="flex: 1; min-width: 200px; text-align: left;">
+            <p class="bottom-underline" style="margin: 0;">Sources</p>
+            <div style="margin-top: 15px;">
+                <a href="#" class="w3-button">Privacy Policy</a>
+            </div>
+            <div>
+                <a href="#" class="w3-button">Terms of Service</a>
+            </div>
+        </div>
+    </div>
+</div>
